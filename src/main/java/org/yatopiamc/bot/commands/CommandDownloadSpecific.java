6b6971502f1b818aba96fef637c79cac00a38dfc
toinouH <antoine-hassler@orange.fr>
2021-04-02 20:12:36 +0200

    more

--- src/main/java/org/yatopiamc/bot/commands/CommandDownloadSpecific.java
+++ src/main/java/org/yatopiamc/bot/commands/CommandDownloadSpecific.java
@@ -12,7 +12,7 @@ import org.yatopiamc.bot.EmbedUtil;
 
 @CommandAliases("download|1.15.2|1.16.1|1.16.2|1.16.3|1.16.4|1.16.5")
 @CommandDescription("Download specific commands.")
-@CommandUsage("download|?1.15.2|?1.16.1|?1.16.2|?1.16.3|?1.16.4|?1.16.5")
+@CommandUsage("download|/1.15.2|/1.16.1|/1.16.2|/1.16.3|/1.16.4|/1.16.5")
 public class CommandDownloadSpecific extends Command {
 
   @Override
@@ -25,7 +25,7 @@ public class CommandDownloadSpecific extends Command {
             .sendMessage(
                 EmbedUtil.withAuthor(context.getAuthor())
                     .setDescription(
-                        "Run `?1.15.2` or `?1.16.1` or `?1.16.2` or `?1.16.3` or `?1.16.4` or `?1.16.5`")
+                        "Run `/1.15.2` or `/1.16.1` or `/1.16.2` or `/1.16.3` or `/1.16.4` or `/1.16.5`")
                     .build())
             .queue();
         break;
